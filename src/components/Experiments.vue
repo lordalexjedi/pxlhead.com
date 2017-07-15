541890e0c3e0b565d3ba65e12cd67ea9797412f4
Liza Kobrazova <lizakobrazova@gmail.com>
2017-07-15 20:27:15 +0300

    Fix searcher

--- src/components/Experiments.vue
+++ src/components/Experiments.vue
@@ -5,14 +5,18 @@
         a.filter-item #CSS
         a.filter-item #JS
         a.filter-item #WebVR
-      .search-box
-        input.search(type='search' name='search' v-model='search')
+      .search-box(@click='searchActive = true')
+        input.search(type='search' name='search' v-model='search'
+                    :class='{ "search--active": searchActive }'
+                    @blur='searchActive = false')
     .gallery
-      .card(v-for='n in 3')
+      .card(v-for='n in 6' @click='experiment = n')
         .card-img(:style='{ backgroundImage: `url(${url})` }')
         .card-info
-          h3.card-name Awesome experiment
-          p.card-description Most awesome experiment description ever
+          h3.card-name Awesome name
+          p.card-description The most awesome experiment description ever.
+
+    .experiment
 </template>
 
 <script>
@@ -21,7 +25,9 @@ export default {
   data() {
     return {
       search: '',
-      url: 'https://www.dl.dropboxusercontent.com/s/rn26nu25g77swmm/nice_smile-wallpaper-1920x1080.jpg?dl=0',
+      searchActive: false,
+
+      url: 'https://www.dl.dropboxusercontent.com/s/f52afi28wevcs9z/smile.jpg?dl=0',
     };
   },
 };
@@ -55,8 +61,6 @@ export default {
   color: $color-white;
   cursor: pointer;
 }
-
-// TODO: fix searcher
 .search-box {
   position: relative;
   cursor: pointer;
@@ -79,14 +83,10 @@ export default {
   outline-style: none;
   font-size: 1.6rem;
   transition: all .5s;
-  &:focus {
-    width: 30rem;
-    padding: 0 3rem;
-  }
 }
 .search--active {
   width: 30rem;
-  padding: 0 3rem;
+  padding: 0 4rem;
 }
 .gallery {
   max-width: 100%;
@@ -96,12 +96,42 @@ export default {
 }
 .card {
   width: 30rem;
-  height: 36rem;
+  height: 34rem;
+  position: relative;
+  margin-bottom: 8rem;
+  cursor: pointer;
+  &:not(:nth-of-type(3n)) {
+    margin-right: 8rem;
+  }
 }
 .card-img {
   background-repeat: no-repeat;
   background-position: center;
   background-size: cover;
-  height: 100%;
+  height: 80%;
+}
+.card-info {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: calc(100% - 2rem);
+  height: calc(30% - 2rem);
+  padding: 1rem;
+  background-color: $color-blue;
+  color: $color-white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+}
+.card-name {
+  font-family: 'Montserrat', sans-serif;
+  font-size: 2rem;
+  font-weight: 600;
+  text-transform: uppercase;
+}
+.card-description {
+  font-size: 1.6rem;
+  margin-top: 1rem;
 }
 </style>

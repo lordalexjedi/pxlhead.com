58c93c8218ab6948b2ca9bfd8b1f6ea72944c111
Alex Shevlyakov <jedi.dart@yandex.ru>
2017-08-26 19:12:46 +0300

    Minor bug fixes

--- src/views/single/ArticleView.vue
+++ src/views/single/ArticleView.vue
@@ -11,11 +11,6 @@
             a.article-view-btn COMMENT
         a.article-view-link
         a.article-view-esc
-        //- .article-view-nav
-        //-   router-link(v-if='page > 1'  :to='`/${type}/(page - 1)`') &lt; prev
-        //-   a.disabled(v-else) &lt; prev
-        //-   router-link(v-if='hasMore'  :to='`/${type}/(page + 1)`') more &gt;
-        //-   a.disabled(v-else) more &gt;
       .article-read
         .text-block(v-html='item.text')
         .article-read-social
@@ -52,6 +47,18 @@ export default {
 
     comment: false
   }),
+
+  computed: {
+    item () {
+      return this.$store.state.items[this.$route.params.id]
+    },
+    page () {
+      return Number(this.$store.state.route.params.page) || 1
+    },
+    hasMore () {
+      return this.page < this.maxPage
+    }
+  },
 
   asyncData ({ store, route: { params: { id }}}) {
     return store.dispatch('FETCH_ITEMS', { ids: [id] })

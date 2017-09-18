9ea9e43936a58d6a5073fa322780b9ff726b6a7d
Liza Kobrazova <lizakobrazova@gmail.com>
2017-09-18 17:34:14 +0300

    Add menu planets with links

--- src/components/AppMenu.vue
+++ src/components/AppMenu.vue
@@ -4,9 +4,11 @@
     span.app-menu-title PXLHEAD
 
     transition(name='push')
-      .app-menu-modal(v-show='open' @click='changePage')
-        h2.app-menu-link {{ activeLink }}
+      .app-menu-modal(v-show='open' @click='changePage'
+        :class='{ pointer: showPointer }')
         a.app-menu-esc(@click='open = !open')
+        h2.app-menu-lable(v-show='topView' v-for='label in labels'
+          :style='{ top: label.top, left: label.left }') {{ label.name }}
 </template>
 
 <script>
@@ -17,17 +19,11 @@ import {
   WebGLRenderer,
   PCFSoftShadowMap,
   Raycaster,
-  AmbientLight,
+  DirectionalLight,
   Vector2,
-  Vector3,
-  Mesh,
-  Group,
-  Geometry,
-  PointsMaterial,
-  Points,
-  SphereGeometry,
-  MeshLambertMaterial
+  Group
 } from 'three'
+import { drawSpace, drawPlanet, drawOrbit } from '@/util/space'
 
 export default {
   name: 'app-menu',
@@ -35,22 +31,10 @@ export default {
   data() {
     return {
       open: false,
+      topView: false,
+      showPointer: false,
       activeLink: '',
-
-      scene: null,
-      camera: null,
-      renderer: null,
-      raycaster: null,
-      mouse: null,
-      starSystem: null,
-      head: null,
-      socials: null,
-
-      headStars: [],
-      socialStars: [],
-
-      width: 0,
-      height: 0,
+      labels: []
     }
   },
 
@@ -69,12 +53,11 @@ export default {
   methods: {
     init() {
       this.camera = new PerspectiveCamera(75, this.width / this.height, 0.1, 1000)
-      this.camera.position.z = 300
+      this.camera.position.set(0, -20, 300)
 
       this.renderer = new WebGLRenderer({ alpha: true, antialias: true })
       this.renderer.setPixelRatio(window.devicePixelRatio)
       this.renderer.setSize(this.width, this.height)
-      this.renderer.setClearColor(0x0E2255)
       this.renderer.shadowMap.enabled = true
       this.renderer.shadowMap.type = PCFSoftShadowMap
 
@@ -85,189 +68,111 @@ export default {
       container.appendChild(this.renderer.domElement)
 
       window.addEventListener('resize', this.onResize)
+      window.addEventListener('wheel', this.onWheel)
     },
     draw() {
-      this.scene.add(new AmbientLight())
-
-      this.starSystem = new Group()
+      // draw stars
+      this.starSystem = drawSpace()
       this.scene.add(this.starSystem)
 
-      this.starSystem.add(this.createStarSystem(200, 0xFFFFFF, 0.8, 250))
-      this.starSystem.add(this.createStarSystem(200, 0x4BD6C6, 1, 200))
+      // draw planets with page links
+      this.orbitSystem = new Group()
+      this.orbitSystem.rotation.x = -1.2
+      this.scene.add(this.orbitSystem)
 
-      this.head = new Group()
-      this.head.scale.set(1.2, 1.2, 1.2)
-      this.scene.add(this.head)
+      this.planets = []
 
-      // draw main links
-      const headLinkParams = [
-        { name: 'projects', position: [-70, 70] },
-        { name: 'articles', position: [70, 70] },
-        { name: 'music', position: [70, 0] },
-        { name: 'art', position: [40, -70] },
-        { name: 'experiments', position: [-70, -70] },
-        { name: 'intro', position: [10, 25] },
-        { name: 'middle', position: [40, 0] }
+      const mainPlanet = drawPlanet({ color: 0xD8DF72, size: 28 })
+      mainPlanet.name = 'intro'
+      this.scene.add(mainPlanet)
+      this.planets.push(mainPlanet)
+
+      this.planetsData = [
+        { name: 'art', color: 0xD72DA9, size: 9, speed: 0.002, radius: 50 },
+        { name: 'music', color: 0xB42DD7, size: 10, speed: 0.001, radius: 80 },
+        { name: 'articles', color: 0x1DC3B0, size: 11, speed: 0.0008, radius: 110 },
+        { name: 'projects', color: 0x3CA1E7, size: 14, speed: 0.0006, radius: 140 },
+        { name: 'experiments', color: 0x5137EF, size: 15, speed: 0.0004, radius: 170 }
       ]
-      headLinkParams.forEach(link => {
-        const star = this.drawLinkStar(link.name, link.position, 5)
-        this.head.add(star)
-        this.headStars.push(star)
+      this.planetsData.forEach(data => {
+        const planet = drawPlanet(data)
+        planet.name = data.name
+
+        const orbit = drawOrbit(data.radius)
+        orbit.speed = data.speed
+        this.orbitSystem.add(orbit)
+
+        const xPos = Math.random() * data.radius * 2 - data.radius
+        const factor = Math.random() < 0.5 ? -1 : 1
+        const yPos = Math.sqrt(Math.pow(data.radius, 2) - Math.pow(xPos, 2)) * factor
+        planet.position.set(xPos, yPos, 0)
+        planet.name = data.name
+        orbit.add(planet)
+        this.planets.push(planet)
+
+        this.labels.push({
+          name: data.name,
+          radius: data.radius
+        })
       })
 
-      // draw star lines between links
-      const headLineParams = [
-        [this.headStars[0].position, this.headStars[1].position, 16],
-        [this.headStars[0].position, this.headStars[4].position, 16],
-        [this.headStars[1].position, this.headStars[2].position, 8],
-        [this.headStars[6].position, this.headStars[2].position, 4],
-        [this.headStars[6].position, this.headStars[3].position, 8],
-        [this.headStars[4].position, this.headStars[3].position, 12]
-      ]
-      headLineParams.forEach((param) => {
-        this.head.add(this.drawStarLine(1, ...param))
-        this.head.add(this.drawStarLine(0.5, ...param))
-      })
+      // draw lights
+      const light1 = new DirectionalLight(0xffffff, 1.3)
+      light1.position.set(-50, -50, 50)
+      light1.castShadow = true
+      mainPlanet.add(light1)
 
-      this.socials = new Group()
-      this.socials.position.set(-300, -40, 0)
-      this.scene.add(this.socials)
-
-      const socialLinkParams = [
-        { name: 'github', position: [0, 0] },
-        { name: 'twitter', position: [20, 100] },
-        { name: 'facebook', position: [40, -70] },
-        { name: 'dribble', position: [-10, -120] }
-      ]
-      socialLinkParams.forEach(link => {
-        const star = this.drawLinkStar(link.name, link.position, 4)
-        this.socials.add(star)
-        this.socialStars.push(star)
-      })
-
-      const socialLineParams = [
-        [this.socialStars[0].position, this.socialStars[1].position, 8],
-        [this.socialStars[0].position, this.socialStars[2].position, 8],
-        [this.socialStars[2].position, this.socialStars[3].position, 8]
-      ]
-      socialLineParams.forEach((param) => {
-        this.socials.add(this.drawStarLine(0.8, ...param))
-        this.socials.add(this.drawStarLine(0.3, ...param))
-      })
-    },
-    createStarSystem(density, color, size, spread) {
-      const stars = new Geometry()
-      const material = new PointsMaterial({ color, size })
-
-      const randPos = () => Math.random() * spread * 2 - spread
-      for (let i = 0; i < density; i++) {
-        const star = new Vector3(randPos(), randPos(), randPos())
-        stars.vertices.push(star)
-      }
-
-      return new Points(stars, material)
-    },
-    drawLinkStar(name, position, size) {
-      const star = new Group()
-      star.position.x = star.origX = position[0]
-      star.position.y = star.origY = position[1]
-      star.name = name
-
-      if (name === 'middle') size = 0.5
-
-      const frontGeometry = new SphereGeometry(size, 16, 16)
-      const backGeometry = new SphereGeometry(size * 1.5, 16, 16)
-      const frontMaterial = new MeshLambertMaterial()
-      const backMaterial = new MeshLambertMaterial({
-        transparent: true,
-        opacity: 0.2
-      })
-
-      const backStar = new Mesh(backGeometry, backMaterial)
-      star.add(backStar)
-      const frontStar = new Mesh(frontGeometry, frontMaterial)
-      star.add(frontStar)
-
-      if (name !== 'intro') this.moveStar(star)
-
-      return star
-    },
-    drawStarLine(size, from, to, density) {
-      const line = new Group()
-
-      const geometry = new SphereGeometry(size, 16, 16)
-      const material = new MeshLambertMaterial()
-
-      const axis = Math.abs(from.x - to.x) > Math.abs(from.y - to.y) ? 'x' : 'y'
-      const mainStep = (to[axis] - from[axis]) / density
-      const sideStep = axis == 'x'
-      ? (to.y - from.y) / density
-      : (to.x - from.x) / density
-
-      for (let i = 1; i <= density; i++) {
-        const star = new Mesh(geometry, material)
-
-        star.position.x = star.origX = axis === 'x'
-        ? from.x + mainStep * i : from.x + sideStep * i
-        star.position.y = star.origY = axis === 'y'
-        ? from.y + mainStep * i : from.y + sideStep * i
-        line.add(star)
-
-        this.moveStar(star)
-      }
-
-      return line
-    },
-    moveStar(star) {
-      const randDist = (orig) => orig + Math.random() * 14 - 7
-      // const randScale = Math.random() * 0.8 + 0.8
-      // TweenLite.to(star.scale, Math.random() + 0.8,
-      //   { x: randScale, y: randScale, z: randScale }
-      // )
-      TweenLite.to(star.position, Math.random() + 2,
-        { x: randDist(star.origX),
-          y: randDist(star.origY),
-          z: 0,
-          onComplete: this.moveStar,
-          onCompleteParams: [star]
-        }
-      )
+      const light2 = new DirectionalLight(0xffffff, 0.8)
+      light2.position.set(80, 50, 50)
+      light2.castShadow = true
+      this.scene.add(light2)
     },
     onMouseMove(event) {
       this.mouse.x = (event.clientX / window.innerWidth) * 2 - 1
       this.mouse.y = -(event.clientY / window.innerHeight) * 2 + 1
 
       this.raycaster.setFromCamera(this.mouse, this.camera)
-      const intersects = this.raycaster.intersectObjects(
-        [...this.headStars, ...this.socialStars],
-        true)
+      const metOrbits = this.raycaster.intersectObjects(this.orbitSystem.children)
+      const metPlanets = this.raycaster.intersectObjects(this.planets)
 
-      if (intersects.length > 0) {
-        const star = intersects[0].object
-        TweenLite.to(star.scale, 0.8, { x: 1.8, y: 1.8, z: 1.8 })
-        TweenLite.to(star.scale, 0.8, { x: 1, y: 1, z: 1, delay: 0.8 })
+      if (metOrbits.length > 0) {
+        const orbit = metOrbits[0].object
+        TweenLite.to(orbit.material, 1.5, { opacity: 0.8 })
+        TweenLite.to(orbit.material, 1.5, { opacity: 0.2, delay: 1.5 })
+      }
 
-        if (this.activeLink !== star.parent.name) this.activeLink = star.parent.name
+      if (metPlanets.length > 0) {
+        const planet = metPlanets[0].object
+        this.activeLink = planet.name
+        this.showPointer = true
+      } else {
+        this.activeLink = ''
+        this.showPointer = false
+      }
+    },
+    onWheel(event) {
+      if (event.deltaY < 0) {
+        TweenLite.to(this.orbitSystem.rotation, 2, { x: -1.2 })
+        this.topView = false
+      } else {
+        TweenLite.to(this.orbitSystem.rotation, 2, { x: 0 })
+        this.topView = true
       }
     },
     changePage() {
-      this.raycaster.setFromCamera(this.mouse, this.camera)
-      const intersects = this.raycaster.intersectObjects(
-        [...this.headStars, ...this.socialStars],
-        true)
-
-      if (intersects.length > 0) {
-        const type = intersects[0].object.parent.name
-        this.$router.push(`/${type}`)
+      if (this.activeLink) {
+        this.$router.push(`/${this.activeLink}`)
         this.open = false
       }
     },
     animate() {
       requestAnimationFrame(this.animate)
 
-      if (this.open) {
-        this.starSystem.children[0].rotation.y += 0.0005
-        this.starSystem.children[1].rotation.y -= 0.0005
+      if (this.open && this.starSystem) {
+        // this.starSystem.rotation.z -= 0.0004
+        this.orbitSystem.children.forEach(orbit => {
+          orbit.rotation.z += orbit.speed
+        })
 
         this.renderer.render(this.scene, this.camera)
       }
@@ -275,18 +180,23 @@ export default {
     setSize() {
       this.width = window.innerWidth
       this.height = window.innerHeight
+
+      this.labels.forEach(label => {
+        label.top = this.height / 2 - 38 + label.radius * 1.8 + 'px'
+        label.left = this.width / 2 - 50 + 'px'
+      })
     },
     onResize() {
       this.setSize()
       this.camera.aspect = this.width / this.height
       this.camera.updateProjectionMatrix()
       this.renderer.setSize(this.width, this.height)
-    },
-
+    }
   },
 
   beforeDestroy() {
     window.removeEventListener('resize', this.onResize)
+    window.removeEventListener('wheel', this.onWheel)
   }
 }
 </script>
@@ -379,8 +289,8 @@ export default {
 }
 .app-menu-modal {
   display: block;
-  width: 100%;
-  height: 100%;
+  background-color: #14003C;
+  overflow: hidden;
 }
 .app-menu-esc {
   position: absolute;
@@ -420,15 +330,19 @@ export default {
     transition: 0.3s ease-in-out;
   }
 }
-.app-menu-link {
+.app-menu-lable {
   display: block;
   position: absolute;
-  bottom: 10rem;
-  left: calc(50% - 4rem / 2);
-  font-size: 3rem;
+  font-size: 16px;
+  width: 100px;
   text-transform: uppercase;
-  font-weight: 600;
+  text-align: center;
   color: $color-white;
+  transition: all 0.3s;
+  opacity: 0.8;
+}
+.pointer {
+  cursor: pointer;
 }
 
 @keyframes sonar {

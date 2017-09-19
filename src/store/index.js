bc873f923620b68d6e74d84dc30f289cb3ebbdc4
Liza Kobrazova <lizakobrazova@gmail.com>
2017-09-19 10:46:57 +0300

    Change itemsPerSlice

--- src/store/index.js
+++ src/store/index.js
@@ -12,7 +12,7 @@ export function createStore() {
       activeType: null,
       activeSort: 'views',
       activeSlice: 1,
-      itemsPerSlice: 5,
+      itemsPerSlice: 12,
       items: {/* [id: number]: Item */},
       comments: {/* [id: number]: Comment */},
       lists: {

4fec9b155249936f8b1a066db245e36ca939ba9d
Alex Shevlyakov <jedi.dart@yandex.ru>
2017-09-23 15:10:44 +0300

    Use google materal icons and add tooltips in menu

--- src/App.vue
+++ src/App.vue
@@ -20,6 +20,7 @@ export default {
 @import url('https://fonts.googleapis.com/css?family=Montserrat:600,700');
 @import url('https://fonts.googleapis.com/css?family=Roboto:400');
 @import url('https://fonts.googleapis.com/css?family=Roboto+Slab:400,700');
+@import url('https://fonts.googleapis.com/icon?family=Material+Icons');
 
 * {
   margin: 0;
@@ -36,7 +37,30 @@ body {
 .hide-scrollbar {
   overflow: hidden;
 }
+.material-icons {
+  font-family: 'Material Icons';
+  font-weight: normal;
+  font-style: normal;
+  font-size: 24px;  /* Preferred icon size */
+  display: inline-block;
+  line-height: 1;
+  text-transform: none;
+  letter-spacing: normal;
+  word-wrap: normal;
+  white-space: nowrap;
+  direction: ltr;
 
+  /* Support for all WebKit browsers. */
+  -webkit-font-smoothing: antialiased;
+  /* Support for Safari and Chrome. */
+  text-rendering: optimizeLegibility;
+
+  /* Support for Firefox. */
+  -moz-osx-font-smoothing: grayscale;
+
+  /* Support for IE. */
+  font-feature-settings: 'liga';
+}
 
 .menu-transtion-enter-active, .menu-transtion-leave-active {
   transition: opacity .5s
